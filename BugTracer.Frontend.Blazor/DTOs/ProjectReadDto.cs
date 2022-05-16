127e3c493283c29f13a3e060540e6b27759a113b
lzlocki <lzlocki@poczta.fm>
2022-05-16 22:41:06 +0200

    Add needed DTOs

---
+++ BugTracer.Frontend.Blazor/DTOs/ProjectReadDto.cs
@@ -0,0 +1,18 @@
+﻿ using System.ComponentModel.DataAnnotations;
+
+namespace BugTracer.Api.Dtos
+{
+    public class ProjectReadDto
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ClosedOn { get; set; }
+        public string ClosedBy { get; set; }
+
+        // nav properties
+        public List<TicketReadDto> TicketsDto { get; set; }
+    }
+}
---
+++ BugTracer.Frontend.Blazor/DTOs/TicketReadDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BugTracer.Api.Dtos
+{
+    public class TicketReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ClosedOn { get; set; }
+        public string ClosedBy { get; set; }
+
+        // Foreign keys
+        public int StatusId { get; set; }
+        public int PriorityId { get; set; }
+        public int ResourceId { get; set; }
+        public int ProjectId { get; set; }
+        
+    }
+}
