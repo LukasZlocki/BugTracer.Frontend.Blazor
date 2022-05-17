32575b382079025e986c5fde559690e53b988766
lzlocki <lzlocki@poczta.fm>
2022-05-17 20:46:30 +0200

    Add view model

---
+++ BugTracer.Frontend.Blazor/ViewModels/ProjectTicketsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BugTracer.Frontend.Blazor;
+using BugTracer.Frontend.Blazor.DTOs;
+
+namespace BugTracer.Frontend.Blazor.ViewModels
+{
+    public class ProjectTicketsViewModel
+    {
+        public ProjectBasicReadDto  ProjectBasicDataReadDto { get; set; } = new ProjectBasicReadDto();
+        public List<TicketReadDto> ProjectTicketsReadDto { get; set; } = new List<TicketReadDto>();
+
+        //  CONSTR
+        public ProjectTicketsViewModel()
+        {
+
+        }
+
+        // constructor
+        public ProjectTicketsViewModel(ProjectBasicReadDto project, List<TicketReadDto> ticketsList)
+        {
+            ProjectBasicDataReadDto = project;
+            ProjectTicketsReadDto = ticketsList;
+        }
+
+
+    }
+}
